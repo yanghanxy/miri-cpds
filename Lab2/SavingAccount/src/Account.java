7daa391095ded444edcfc5c281d733516a3b97b2
Calvin <calvin.bulla@gmail.com>
2015-10-13 18:16:55 +0200

    Added implementation for Saving Account

--- Lab2/SavingAccount/src/Account.java
+++ Lab2/SavingAccount/src/Account.java
@@ -1,14 +1,25 @@
 public class Account {
-	int balance = 0;
-
-	public Account() {
-	}
+	// This account is unbounded
+	private int balance = 0;
 
 	public synchronized void deposit(int amount) {
-		//TODO +
+		// More money
+		balance += amount;
+		System.out.println("+ $" + amount + " (" + Thread.currentThread().getName() + ")");
+		notifyAll();
 	}
 
-	public synchronized void withdraw(int amount) {
-		//TODO -
+	public synchronized void withdraw(int amount) throws InterruptedException {
+		// Only withdraw if enough money left
+		while (balance <= amount) {
+			System.out.println("Not enough money (" + Thread.currentThread().getName() + ")");
+			wait();
+		}
+		balance -= amount;
+		System.out.println("- $" + amount + " (" + Thread.currentThread().getName() + ")");
+	}
+
+	public synchronized int getBalance() {
+		return balance;
 	}
 }
