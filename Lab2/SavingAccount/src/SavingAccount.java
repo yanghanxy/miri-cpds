9bee3d23f948cfd781fa693c8ccd6d1e2acf872e
yanghanxy <yanghanzzz@gmail.com>
2015-10-15 18:11:17 +0200

    Roll back
    sorry I didn't do git pull before I commit the code
    Now roll back to 2 days ago...

--- Lab2/SavingAccount/src/SavingAccount.java
+++ Lab2/SavingAccount/src/SavingAccount.java
@@ -1,13 +1,29 @@
 public class SavingAccount {
 
 	public static void main(String args[]) {
-		Account account = new Account();
+		final Account account = new Account();
 
 		Thread calvin = new Person(account, "Calvin");
 		Thread han = new Person(account, "Han");
 
-		calvin.start(); 
-		han.start();
+		// UPC scholarship prevents deadlocks
+		new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(10000);
+						System.out.println("==================");
+						System.out.println("= Annual summary =");
+						System.out.println("==================");
+						System.out.println("Current balance: $" + account.getBalance());
+						System.out.println("More money plz");
+						account.deposit(10);
+					} catch (InterruptedException e) {};
+				}
+			}
+		}, "Scholarship").start();
+
+		calvin.start(); han.start();
 	}
 
 }
