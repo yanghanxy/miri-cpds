6821732438df6319cb560cd65d3887b9032dac4a
yanghanxy <yanghanzzz@gmail.com>
2015-10-15 18:01:25 +0200

    Code for Saving Account

--- Lab2/SavingAccount/src/Hola.java
+++
@@ -1,3 +0,0 @@
-public class Hola {
-	//To test
-}
--- Lab2/SavingAccount/src/SavingAccount.java
+++ Lab2/SavingAccount/src/SavingAccount.java
@@ -1,29 +1,13 @@
 public class SavingAccount {
 
 	public static void main(String args[]) {
-		final Account account = new Account();
+		Account account = new Account();
 
 		Thread calvin = new Person(account, "Calvin");
 		Thread han = new Person(account, "Han");
 
-		// UPC scholarship prevents deadlocks
-		new Thread(new Runnable() {
-			public void run() {
-				while (true) {
-					try {
-						Thread.sleep(10000);
-						System.out.println("==================");
-						System.out.println("= Annual summary =");
-						System.out.println("==================");
-						System.out.println("Current balance: $" + account.getBalance());
-						System.out.println("More money plz");
-						account.deposit(10);
-					} catch (InterruptedException e) {};
-				}
-			}
-		}, "Scholarship").start();
-
-		calvin.start(); han.start();
+		calvin.start(); 
+		han.start();
 	}
 
 }
